7415d2edbeb2a5d3eae95950612fba7fc252502c
whitelizard <esbblo@gmail.com>
2019-02-13 00:03:40 +0100

    WIP

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import _ from 'lodash';
 import * as R from 'ramda';
 // import { createStore } from 'redux';
 
-export const mlParser = ({ envExtra = {}, keepJsEval = false }, vars) => {
+export const minimalLispParser = ({ envExtra = {}, keepJsEval = false }, vars) => {
   const parser = miniMAL({
     undefined,
     typeof: a => typeof a, // renaming of miniMAL's 'type'
@@ -85,50 +85,66 @@ const initialRuleState = {
   active: false,
   flipped: false,
   lastFired: 0,
-  vars: {},
+  // vars: {},
 };
 
-export const loadRule = (ruleConf, envExtra, idKey = 'id', triggerHook) => {
+export const loadRule = (ruleConf, envExtra, idKey = 'id', patchParser) => {
   const { [idKey]: id, triggers = [], actuator = 'backend', active } = ruleConf;
   if (actuator !== 'backend') return;
   ruleStore[id] = { ...initialRuleState, ...(ruleStore[id] || {}), conf: ruleConf, active };
-  const { vars } = ruleStore[id];
-  const ml = mlParser({ envExtra }, vars);
+  // const { vars } = ruleStore[id];
+  const vars = {};
+  const parser = minimalLispParser({ envExtra }, vars);
   triggers.forEach(trigger => {
-    if (typeof trigger === 'object') {
+    if (typeof trigger === 'object' && !Array.isArray(trigger)) {
       trigger.entries().forEach(([key, val]) => {
-        if (triggerHook) triggerHook(ml, vars, key);
-        ml.eval(val);
+        if (patchParser) patchParser(parser, vars, key);
+        parser.eval(val);
       });
     } else {
-      if (triggerHook) triggerHook(ml, vars);
-      ml.eval(trigger);
+      if (patchParser) patchParser(parser, vars);
+      parser.eval(trigger);
     }
   });
 };
 
-const checkRule = (ruleConf, active, lastFired, flipped) => {
-  if (!active) return {};
+const checkRule = id => {
+  const { active, lastFired, flipped, conf } = ruleStore[id];
+  if (!active) return true;
   const now = Date.now();
-  const { cooldown = 0, resetCondition, ttl = Infinity } = ruleConf;
-  if (ttl && ttl < now) return { active: false };
-
+  const { cooldown = 0, resetCondition, ttl = Infinity } = conf;
+  if (ttl && ttl < now) {
+    ruleStore[id] = { ...(ruleStore[id] || {}), active: false };
+    return true;
+  }
   const cooledDown = now >= lastFired + cooldown;
-  if (flipped && !resetCondition && !cooledDown) return {};
-  return undefined; // continue
+  if (flipped && !resetCondition && !cooledDown) return true;
+  return false; // continue
 };
 
-const processRule = (id, ml, vars) => {
-  const { conf, active, lastFired, flipped } = ruleStore[id];
-  const newState = checkRule(conf, active, lastFired, flipped);
-  if (newState) {
-    ruleStore[id] = { ...(ruleStore[id] || {}), ...newFinalState };
-    return;
-  } // else continue
-  const { asyncs, process } = rule;
+const handleAsyncs = async (parser, result, asyncConf = []) => {
+  if (R.isEmpty(asyncConf)) return result;
+  const [nextAsync, ...rest] = asyncConf;
+  if (typeof nextAsync === 'object' && !Array.isArray(nextAsync)) {
+    const keyPromisePairs = Object.entries(nextAsync);
+    const results = await Promise.all(keyPromisePairs.map(([_, val]) => parser.eval(val)));
+    results.forEach((data, ix) => {
+      result[keyPromisePairs[ix][0]] = data;
+    });
+  } else {
+    await parser.eval(nextAsync);
+    return handleAsyncs(parser, result, rest);
+  }
+};
+
+export const processRule = (id, parser, vars) => {
+  const { conf, flipped } = ruleStore[id];
+  if (checkRule(id)) return;
+  const { asyncs, process } = conf;
+  await handleAsyncs(parser, vars, asyncs);
   // const fetched = preFetch(fetcher, preFetches);
 
-  const vars = Array.isArray(process) ? 1 : ml.eval(rule.process, ctx);
+  const vars = Array.isArray(process) ? 1 : parser.eval(rule.process, ctx);
 
   if (flipped && resetCondition) {
     const conditionsMet = conditionTransform(resetCondition, context);
